657b2c58016eaa7a3ef958d113450c8e4ab0296b
jonahmakori <jonahmakori0@gmail.com>
2019-12-01 16:14:58 +0300

    modified js succesifully

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,15 @@ var submission= function() {
     var day = parseInt(document.getElementById("day").value);
     var month = parseInt(document.getElementById("month").value);
     var year = parseInt(document.getElementById("year").value);
-    var male = document.getElementById("male");
-    var female = document.getElementById("female");
+    var male = document.getElementsByName("gender");
+    var female = document.getElementsByName("gender");
     var done = document.getElementById("done");
     var dateOfTheWeek = new Date (year + "/" + month + "/" + day);
     var result = dateOfTheWeek.getDay();
 
 
     var day =["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
-    var male =["Kwasi","Kwadwo","Kwabena","KwakuYaw","Kofi","Kwame"];
+    var male =["Kwasi","Kwadwo","Kwabena","KwakuYaw", "Yaw","Kofi","Kwame",];
     var female =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
     if (month < 1 || month > 12) {
@@ -26,11 +26,15 @@ var submission= function() {
      else if (year < 1900) {
       done.innerHTML = "Invalid year please try again";
      }
-     else if (male.checked){
+  
+    else if (gender[0].checked==true){
          done.innerHTML="You were born on" + day[result] + "and your akan name is " + male[result]
      }
-    else {
+    else if (gender[1].checked==true){
         done.innerHTML= "You were born on " + day[result] + "and your akan name is"+ female[result]
     }
+    else{
+      console.log("success");
+    }
 
-};
+}
