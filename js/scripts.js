5462927a67fe0c1cdb3746e455379c83febdc16b
jonahmakori <jonahmakori0@gmail.com>
2019-12-01 23:39:15 +0300

    modified js

--- js/scripts.js
+++ js/scripts.js
@@ -15,23 +15,23 @@ var submission= function() {
     var male =["Kwasi","Kwadwo","Kwabena","KwakuYaw", "Yaw","Kofi","Kwame",];
     var female =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
-    if (month < 1 || month > 12 || "/") {
+    if (month < 1 || month > 12 && "/"  ) {
         done.innerHTML = "Invalid month please try again";
       }
     
-      else if (day < 1 || day > 31 || "/") {
+      else if (day < 1 || day > 31 && "/"  ) {
         done.innerHTML ="Invalid date please try again";
       }
     
-     else if (year < 1900 || "/") {
+     else if (year < 1900  && "/") {
       done.innerHTML = "Invalid year please try again";
      }
   
     else if (gender[0].checked==true){
-         done.innerHTML="You were born on" + day[result] + "and your akan name is " + male[result]
+         done.innerHTML="Hey <b>Boy-Child</b> You were born on  a  " + day[result] + "  and your Akan name Happens to be  " + male[result]
      }
     else if (gender[1].checked==true){
-        done.innerHTML= "You were born on " + day[result] + "and your akan name is"+ female[result]
+        done.innerHTML= "Hey <b>Girl-Child</b> you were born on a  " + day[result] + " and your Akan name Happens to be "+ female[result]
     }
     else{
       console.log("success");
